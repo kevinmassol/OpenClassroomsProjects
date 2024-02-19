096261b9e58abb71d60e033538fd291445ce62a5
Kevin Massol <kevin.massol@icloud.com>
2024-02-19 19:32:57 +0100

    Initial Commit

---
+++ OpenClassroomsProjects/OpenClassroomsProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenClassroomsProjectsApp.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenClassroomsProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
