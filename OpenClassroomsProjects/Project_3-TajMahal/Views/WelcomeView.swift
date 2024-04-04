efc043ddfcba979f64af866fb2e6379fd35cec01
Kevin Massol <kevin.massol@icloud.com>
2024-04-04 07:03:47 +0200

    [FEAT #4](project_3) - add image in WelcomeView

--- OpenClassroomsProjects/Project_3-TajMahal/Views/WelcomeView.swift
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/WelcomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Image(.tajMahal)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 335, height: 423)
+            Spacer()
+        }
     }
 }
 
