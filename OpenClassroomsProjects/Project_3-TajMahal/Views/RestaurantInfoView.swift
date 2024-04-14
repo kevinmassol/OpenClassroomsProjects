11e54d97e7b19b07319f3ed95e3514db3baee14d
Kevin Massol <kevin.massol@icloud.com>
2024-04-14 14:17:43 +0200

    [FEAT #5](project_3) - add fonts

---
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/RestaurantInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantInfoView.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 04/04/2024.
+//
+
+import SwiftUI
+
+struct RestaurantInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantInfoView()
+}
