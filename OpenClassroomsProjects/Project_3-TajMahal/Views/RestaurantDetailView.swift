176a8da8e71f47a37c3e642f3ac12882c30ca467
Kevin Massol <kevin.massol@icloud.com>
2024-04-20 17:45:09 +0200

    [FEAT #8](project_3) - create RestaurandDetailView

---
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/RestaurantDetailView.swift
@@ -0,0 +1,32 @@
+//
+//  RestaurantDetailView.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 20/04/2024.
+//
+
+import SwiftUI
+
+struct RestaurantDetailView: View {
+    
+    let detailData: RestaurantDetail
+    
+    var body: some View {
+        HStack {
+            Image(detailData.icon)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 12, height: 12)
+            Text(detailData.leftText)
+            Spacer()
+            Text(detailData.rightText)
+        }
+        .font(.custom(TextFont.font600, size: 12))
+        .frame(width: 335, height: 15)
+        .foregroundStyle(.customLightGray)
+    }
+}
+
+#Preview {
+    RestaurantDetailView(detailData: details[0])
+}
