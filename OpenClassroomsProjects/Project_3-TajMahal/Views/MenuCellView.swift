a171ab27a596c9bcc769d11e44584128ff12806d
Kevin Massol <kevin.massol@icloud.com>
2024-04-24 06:56:49 +0200

    [FEAT #11](project_3) - create PepperView

---
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/MenuCellView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuCellView.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 24/04/2024.
+//
+
+import SwiftUI
+
+struct MenuCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuCellView()
+}
---
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/PepperView.swift
@@ -0,0 +1,41 @@
+//
+//  PepperView.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 24/04/2024.
+//
+
+import SwiftUI
+
+struct PepperView: View {
+    
+    let spiceLevel: SpiceLevel
+    
+    func spiceLevelImages(level: SpiceLevel) -> some View {
+        
+        let redPepperImage = Image(.pimentRed)
+            .resizable()
+            .frame(width: 12, height: 12)
+        let grayPepperImage = Image(.piment)
+            .resizable()
+            .frame(width: 12, height: 12)
+        
+        return HStack(spacing: 6) {
+            ForEach(0..<level.rawValue, id: \.self) { spiceLevel in
+                redPepperImage
+            }
+            ForEach(level.rawValue..<SpiceLevel.hot.rawValue, id: \.self) { spiceLevel in
+                grayPepperImage
+            }
+        }
+    }
+    
+    var body: some View {
+        spiceLevelImages(level: spiceLevel)
+            .frame(width: 58, height: 14)
+    }
+}
+
+#Preview {
+    PepperView(spiceLevel: .light)
+}
