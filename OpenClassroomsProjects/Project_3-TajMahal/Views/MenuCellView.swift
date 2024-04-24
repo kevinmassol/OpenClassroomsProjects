857b8674a0649a16fdd8569e336ffae8d5ea43a9
Kevin Massol <kevin.massol@icloud.com>
2024-04-24 07:00:21 +0200

    [FEAT #12](project_3) - create MenuCellView and add PepperView to this dish cell

--- OpenClassroomsProjects/Project_3-TajMahal/Views/MenuCellView.swift
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/MenuCellView.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct MenuCellView: View {
+    
+    let dish: Dish
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 18) {
+            Image(dish.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 112, height: 86)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            VStack(alignment:.leading, spacing: 10) {
+                Text(dish.name)
+                    .font(.custom(TextFont.font600, size: 14))
+                    .foregroundStyle(.customLightGray)
+                Text(dish.description)
+                    .font(.custom(TextFont.font400, size: 12))
+                    .foregroundStyle(.customGray)
+                HStack {
+                    Text(dish.price)
+                        .font(.custom(TextFont.font600, size: 12))
+                        .foregroundStyle(.customLightGray)
+                    Spacer()
+                    PepperView(spiceLevel: dish.spiceLevel)
+                }
+            }
+        }
+        .frame(width: 335, height: 110)
     }
 }
 
 #Preview {
-    MenuCellView()
+    MenuCellView(dish: TableOfMenus().apetizerArray[0])
 }
