ce48356c20d454523ff288d84aa12cfdcd07afc6
Kevin Massol <kevin.massol@icloud.com>
2024-04-26 22:07:09 +0200

    [FEAT #13](project_3) - add 2 colors in assets

---
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/DescriptionOfTheDish.swift
@@ -0,0 +1,18 @@
+//
+//  DescriptionOfTheDish.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 26/04/2024.
+//
+
+import SwiftUI
+
+struct DescriptionOfTheDish: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DescriptionOfTheDish()
+}
---
+++ OpenClassroomsProjects/Project_3-TajMahal/Views/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  MenuView.swift
+//  OpenClassroomsProjects
+//
+//  Created by KEVIN on 22/04/2024.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
